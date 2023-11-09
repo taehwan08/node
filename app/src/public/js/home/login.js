5a4f93e1d431f1a1185323f5465507baa1331a68
Taehwan <djqtdut13@gmail.com>
2023-11-10 01:19:53 +0900

    dom을 이용해서 html 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,15 @@
+"use strict"
 
-console.log('asdaf')
-console.log("hihihihi")
+const id = document.getElementById("id")
+const psword = document.getElementById("password")
+const button = document.querySelector("button")
+
+button.addEventListener("click", login)
+
+function login(){
+    const req = {
+        id: id.value,
+        password: password.value,
+    }
+    console.log(req)
+}

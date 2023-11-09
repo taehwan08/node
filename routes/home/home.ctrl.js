960641b8dcd9f5de7b44d874142e1f0dd02fc8de
Taehwan <djqtdut13@gmail.com>
2023-11-09 22:24:46 +0900

    node 테스트

---
+++ routes/home/home.ctrl.js
@@ -0,0 +1,14 @@
+"use strict"
+
+function home (req, res) {
+    res.render("home/index")
+}
+
+function login (req, res) {
+    res.render("home/login")
+}
+
+module.exports = {
+    home,
+    login,
+}
